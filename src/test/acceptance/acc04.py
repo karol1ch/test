7769c29085c92b284de517ea5fac4b9c5d9ce869
karol1ch <karol.chomoncik@gmail.com>
2022-08-05 18:49:39 +0200

    Karol Chomoncik; Add validate on heroku

--- src/test/acceptance/acc04.py
+++ src/test/acceptance/acc04.py
@@ -7,8 +7,8 @@ http = urllib3.PoolManager()
 response = http.request('GET', 'http://' + host + '/products/milk')
 if response.status != 200:
     sys.exit(1)
-if response.headers['Content-Type'] != 'application/json;charset=UTF-8':
+greeting = json.loads(response.data.decode('utf-8'))
+if greeting['name'] != 'milk':
     sys.exit(2)
-product = json.loads(response.data.decode('utf-8'))
-if product['name'] != 'milk':
+if greeting['calories'] != 100:
     sys.exit(3)
