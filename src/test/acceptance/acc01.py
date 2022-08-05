ff0f475c1dc6fdbfaed6d3fd42c20d2d9ec355f3
karol1ch <karol.chomoncik@gmail.com>
2022-08-05 17:08:51 +0200

    Karol Chomoncik; Add validate on heroku

---
+++ src/test/acceptance/acc01.py
@@ -0,0 +1,9 @@
+import urllib3
+import json
+import sys
+
+host = sys.argv[1] + '.herokuapp.com' if len(sys.argv) > 1 else 'localhost:8080'
+http = urllib3.PoolManager()
+response = http.request('GET', 'http://' + host + '/products')
+if response.status != 200:
+    sys.exit(1)
