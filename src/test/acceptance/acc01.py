9453188f98e4751f6e8d3f68f8578bda8dafb4af
karol1ch <karol.chomoncik@gmail.com>
2022-08-08 17:09:38 +0200

    Karol Chomoncik; validate deploy

--- src/test/acceptance/acc01.py
+++ src/test/acceptance/acc01.py
@@ -4,6 +4,6 @@ import sys
 
 host = sys.argv[1] + '.herokuapp.com' if len(sys.argv) > 1 else 'localhost:8080'
 http = urllib3.PoolManager()
-response = http.request('GET', 'http://' + host + '/products')
+response = http.request('GET', 'https://' + host + '/products')
 if response.status != 200:
     sys.exit(1)
